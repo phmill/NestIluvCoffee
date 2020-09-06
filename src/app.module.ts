d40685c768678fc70a0276c7353c6e07e5835228
Moulin Philippe <philippe.moulin@gfi.fr>
2020-09-06 12:48:43 +0200

    Dynamic Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
       database: 'myNestDb', // name of our database,
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-    }), CoffeeRatingModule],
+    }), CoffeeRatingModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { createConnection, ConnectionOptions } from 'typeorm';
+
+@Module({})
+export class DatabaseModule {
+  static register(options: ConnectionOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [{
+        provide: 'CONNECTION',
+        useValue: createConnection(options),
+      }]
+    }
+  }
+}
