cde8b1951a78622264df8693706fcf820ad27af4
Moulin Philippe <philippe.moulin@gfi.fr>
2020-09-11 17:09:21 +0200

    asynchronously configure dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,8 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [appConfig]
-    }),
-    CoffeesModule, 
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
       type: 'postgres', // type of our database
       host: process.env.DATABASE_HOST,
       port: +process.env.DATABASE_PORT,
@@ -23,7 +20,14 @@ import appConfig from './config/app.config';
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-    }), CoffeeRatingModule],
+      }),
+    }),
+    ConfigModule.forRoot({
+      load: [appConfig]
+    }),
+    CoffeesModule, 
+    CoffeeRatingModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
