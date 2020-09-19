ecfacc9d006da0ef3f045ef54dae009c75e18a72
Moulin Philippe <philippe.moulin@gfi.fr>
2020-09-19 19:53:53 +0200

    Add Request Logging with Middlleware

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -23,9 +23,7 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  async findAll(@Query() paginationQuery:PaginationQueryDto) {
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
+  findAll(@Query() paginationQuery:PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
   }
 
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -10,4 +11,12 @@ import { ApiKeyGuard } from './guards/api-key.guard';
     useClass: ApiKeyGuard,
   }]
 })
-export class CommonModule {}
+export class CommonModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes(
+      '*'
+      // { path: 'coffees', method: RequestMethod.GET }
+    );
+  }
+
+}
