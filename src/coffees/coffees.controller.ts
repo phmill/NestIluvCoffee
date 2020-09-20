e0811af187845e67cd64f46fc42753b61be79384
Moulin Philippe <philippe.moulin@gfi.fr>
2020-09-20 11:29:44 +0200

    Create Custom Param Decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,6 +13,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 // @UsePipes(ValidationPipe)
 @Controller('coffees')
@@ -23,7 +24,8 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  findAll(@Query() paginationQuery:PaginationQueryDto) {
+  async findAll(@Protocol('https') protocol: string, @Query() paginationQuery:PaginationQueryDto) {
+    console.log(protocol);
     return this.coffeesService.findAll(paginationQuery);
   }
 
